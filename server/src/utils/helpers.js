eb9d645fbff2fd009d07119a7c640a1ed56e4536
Divyam Mishra <divyammishra@Divyams-MacBook-Air.local>
2026-06-26 21:20:38 +0530

    switched smtp config to gmail service

--- server/src/utils/helpers.js
+++ server/src/utils/helpers.js
@@ -63,16 +63,11 @@ exports.sendEmail = async ({ to, subject, html }) => {
       const nodemailer = require("nodemailer");
 
       const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: Number(process.env.SMTP_PORT),
-        secure: false, // important for 587
+        service: "gmail",
         auth: {
           user: process.env.SMTP_EMAIL,
           pass: process.env.SMTP_PASSWORD,
         },
-        tls: {
-          rejectUnauthorized: false
-        }
       });
 
       await transporter.verify();
